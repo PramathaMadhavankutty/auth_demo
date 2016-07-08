9f56072299b665294e62777380ee6ebe6d06bdea
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-08 09:47:18 -0400

    remove some folders and files

---
+++ PycharmProjects/auth_demo/subscriptions_webhook.sh
@@ -0,0 +1,83 @@
+curl -H "Content-Type :application/json" -X POST -d
+'{
+
+
+    "object":
+    {
+        "id": "in_18UcRzFdEg2TlLfMWNt5eU0X",
+        "object": "invoice",
+        "amount_due": 999,
+        "application_fee": null,
+        "attempt_count": 1,
+        "attempted": true,
+        "charge": "ch_18UcRzFdEg2TlLfMATH4UZ4F",
+        "closed": true,
+        "currency": "usd",
+        "customer": "cus_8mAnNZ31rqCOri",
+        "date": 1467897715,
+        "description": null,
+        "discount": null,
+        "ending_balance": 0,
+        "forgiven": false,
+        "lines":
+        {
+            "object": "list",
+            "data":[
+            {
+               "id":"sub_8mAnmUaHyaMq3D",
+                "object": "line_item",
+                "amount": 999,
+                "currency": "usd",
+                "description": null,
+                "discountable": true,
+                "livemode": false,
+                "metadata:{},
+                "period":
+                    {
+                        "start": 1467897715,
+                        "end": 1470576115,
+                    },
+                "plan":
+                    {
+                        "id": "REG_MONTHLY",
+                        "object": "plan",
+                        "amount": 999,
+                        "created": 1467888015,
+                        "currency": "usd",
+                        "interval": "month",
+                        "interval_count": 1,
+                        "livemode": false,
+                        "metadata":{},
+                        "name": "Monthly Subscription",
+                        "statement_descriptor": "Monthly Subscription",
+                        "trial_period_days": null,
+                    },
+                "proration": false,
+                "quantity": 1,
+                "subscription": null,
+                "type": "subscription",
+            }
+            ]
+            "has_more": false,
+            "total_count": 1,
+            "url": "/v1/invoices/in_18UcRzFdEg2TlLfMWNt5eU0X/lines"
+        }
+        "livemode": "false",
+        "metadata":{},
+        "next_payment_attempt": null,
+        "paid": "true",
+        "period_end": 1467897715,
+        "period_start": 1467897715,
+        "receipt_number": "null",
+        "starting_balance": 0,
+        "statement_descriptor": null,
+        "subscription": "sub_8mAnmUaHyaMq3D",
+        "subtotal": 999,
+        "tax": null,
+        "tax_percent": null,
+        "total": 999,
+        "webhooks_delivered_at": null
+    }
+
+
+}'http://localhost:8000/subscriptions_webhook/
