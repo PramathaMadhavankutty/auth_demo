0f9d3d21dd7fd8d347cfcc14f8fce871532d727e
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-10 11:51:13 -0400

    modify changes

--- PycharmProjects/paypal_subscriptions_2/paypal_subscriptions_2/urls.py
+++ PycharmProjects/paypal_subscriptions_2/paypal_subscriptions_2/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from paypal.standard.ipn import urls as paypal_urls
+from paypal_store import views as paypal_views
+from products import views as product_views
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',product_views.get_index, name='index'),
+    url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
+    url(r'^paypal-return$', paypal_views.paypal_return),
+    url(r'^paypal-cancel/$', paypal_views.paypal_cancel),
+    url(r'^products/$',product_views.all_products),
 ]
