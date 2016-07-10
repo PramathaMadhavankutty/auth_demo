37d8c4264fa48e3e05e795f621a2f960d6426fe4
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-10 11:45:21 -0400

    modify views

--- PycharmProjects/paypal_subscriptions_2/products/views.py
+++ PycharmProjects/paypal_subscriptions_2/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "products/products.html",{"products":products})
+def get_index(request):
+    return render(request, "home.html")
