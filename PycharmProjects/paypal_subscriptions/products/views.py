e25cfb2b15050bf0b7b3866422e2fd47b35a1041
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-10 08:09:22 -0400

    add views to homepage

--- PycharmProjects/paypal_subscriptions/products/views.py
+++ PycharmProjects/paypal_subscriptions/products/views.py
@@ -5,3 +5,5 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products/products.html",{"products":products})
+def get_index(request):
+    return render(request, "home.html")
