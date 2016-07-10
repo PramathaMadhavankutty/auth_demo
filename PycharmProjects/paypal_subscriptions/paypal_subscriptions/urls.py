14bdc9046579a8045f95ba44c5ae0587cad9cc00
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-10 07:47:54 -0400

    add url for products

--- PycharmProjects/paypal_subscriptions/paypal_subscriptions/urls.py
+++ PycharmProjects/paypal_subscriptions/paypal_subscriptions/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from paypal.standard.ipn import urls as paypal_urls
 from paypal_store import views as paypal_views
+from products import views as product_views
 from django.contrib import admin
 
 urlpatterns = [
@@ -23,5 +24,6 @@ urlpatterns = [
     url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
     url(r'^paypal_return/$', paypal_views.paypal_return),
     url(r'^paypal_cancel/$', paypal_views.paypal_cancel),
-
+    url(r'^products/$',product_views.all_products),
+    
 ]
