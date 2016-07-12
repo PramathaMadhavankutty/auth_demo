80ccc1a6ce8e0b39cb31008aa2c3ccabaaeca8a9
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-12 15:20:35 -0400

    include accounts in settings

--- PycharmProjects/flatpages_demo/flatpages_demo/settings.py
+++ PycharmProjects/flatpages_demo/flatpages_demo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
+    'accounts',
     'django.contrib.sites',
     'django.contrib.flatpages',
     'paypal_store',
