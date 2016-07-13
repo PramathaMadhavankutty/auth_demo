444876a7c1c6179daa8effa7a919dab2b2f34ff9
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-13 10:44:49 -0400

    create login url

--- We_Are_Social/We_Are_Social/urls.py
+++ We_Are_Social/We_Are_Social/urls.py
@@ -30,5 +30,7 @@ urlpatterns = [
 # Email Auth
     url(r'^register/$',accounts_views.register, name='register'),
     url(r'^profile/$',accounts_views.profile, name='profile'),
+    url(r'^login/$',accounts_views.login, name='login'),
+    url(r'^logout/$',accounts_views.logout, name='logout'),
 
 ]
