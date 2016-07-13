be750bb38f7dc6a6e19b66d0b1ffba7430df6b4c
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-13 10:36:55 -0400

    login and logout views

--- We_Are_Social/accounts/views.py
+++ We_Are_Social/accounts/views.py
@@ -33,3 +33,30 @@ def register(request):
 
 def profile(request):
     return render(request, 'profile.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(email=request.POST.get('email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+                return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your email or password was not recognised")
+
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return render(request, 'index.html')
