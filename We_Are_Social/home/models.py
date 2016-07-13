6598dab716b9529ff128646e4b26adff7fff3a42
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-13 07:44:22 -0400

    create a model

--- We_Are_Social/home/models.py
+++ We_Are_Social/home/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __unicode__(self):
+        return self.title
