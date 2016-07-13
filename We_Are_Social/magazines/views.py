b643b36965bbf03304bc37f0351ab0ef6fd1b748
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-13 13:54:10 -0400

    include magazine views

--- We_Are_Social/magazines/views.py
+++ We_Are_Social/magazines/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Magazine
+from accounts.views import login_required
 
-# Create your views here.
+
+@login_required(login_url='/login/')
+def all_magazines(request):
+    magazines = Magazine.objects.all()
+    return render(request, "magazines/magazines.html", {"magazines": magazines})
