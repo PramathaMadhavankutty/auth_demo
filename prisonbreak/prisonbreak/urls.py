d097a573a66c2b53217589c1a87243032ae56191
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-27 13:19:51 -0400

    add store app, urls and views

--- prisonbreak/prisonbreak/urls.py
+++ prisonbreak/prisonbreak/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls'), name='home'),
+    url(r'^store/', include('store.urls'), name='store')
 ]
