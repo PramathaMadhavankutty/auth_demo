fe8e6cf9603b9097b71aaa8675d87e9df4a08f08
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-27 19:09:16 -0400

    create models views and register the model

--- prisonbreak/store/admin.py
+++ prisonbreak/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'stock')
+
+admin.site.register(Item, ItemAdmin)
--- prisonbreak/store/models.py
+++ prisonbreak/store/models.py
@@ -1,5 +1,11 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=150)
+    season = models.CharField(max_length=150)
+    description = models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    stock = models.IntegerField(default=0)
