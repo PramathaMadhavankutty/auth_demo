4111601f0fcb19da019533116ee27adf3dc7394a
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-27 08:50:48 -0400

    add urls, views and templates

--- prisonbreak/home/views.py
+++ prisonbreak/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'template.html')
+
+
+def home(request):
+    return render(request, 'home.html')
